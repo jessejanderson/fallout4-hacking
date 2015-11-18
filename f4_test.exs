020151e833e497aa8979b5de6fe077472c44e9cf
Jesse J. Anderson <jessejanderson@gmail.com>
2015-11-17 22:13:23 -0800

    create basic tests

--- f4_test.exs
+++ f4_test.exs
@@ -4,19 +4,12 @@ ExUnit.configure(exclude: :pending)
 defmodule F4HackingTest do
   use ExUnit.Case, async: true
 
-  test ""
-end
+  test "hack with no words" do
+    assert Hack.score_words("") == :error
+  end
 
-F4Hacking.hack
-#=> What words are available? (Ex: "apple, blast, climb, etc")
-foo, bar, bot, fat
-#=> Which word did you hack?
-foo
-#=> How much likeness?
-1
-#=> Try one of these: BOT, FAT
-#=> Which word did you hack?
-bot
-#=> How much likeness?
-1
-#=> The correct answer is FAT
+  test "hack and sort 3 letter words" do
+    assert Hack.score_words("foo fat bar")
+      == %{foo: 6, fat: 5, bar: 4}
+  end
+end
