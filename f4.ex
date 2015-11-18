717ef46a6dec1861dd71bb5d760b134d109f0edb
Jesse J. Anderson <jessejanderson@gmail.com>
2015-11-18 11:43:39 -0800

    remove words as they are checked and other refactoring

--- f4.ex
+++ f4.ex
@@ -1,11 +1,40 @@
-defmodule Hack do
-  def new,        do: new IO.gets "Available words? "
-  def new(words), do: words |> String.rstrip |> score_words
+# NEXT: remove words that match likeness
+
+defmodule Fallout do
+  def hack,        do: hack IO.gets "Available words? "
+  def hack(words)  do
+    upcase_words = words |> String.rstrip |> String.upcase
+    scored_words = words |> String.rstrip |> score_words
+    word_to_try = scored_words |> best_word |> suggested_word
+    word_list = words |> String.rstrip |> to_list
+    IO.puts "Suggested word: #{word_to_try}"
+    likeness = IO.gets "Likeness? "
+    if "exit\n" == likeness do
+      :ok
+    else
+      likeness = likeness |> String.rstrip |> String.to_integer
+      add_word(word_to_try, likeness, scored_words, String.rstrip(words))
+    end
+  end
+
+  def suggested_word({word, _val}), do: word
+
+  def add_word(word, likeness, word_map, words) do
+    key = word |> String.rstrip |> String.to_atom
+    word_map = Map.put(word_map, key, likeness)
+    new_words = words |> to_list |> Enum.filter(&(&1 != word)) |> list_to_string
+    IO.puts "--"
+    IO.puts word
+    IO.puts words
+    IO.puts new_words
+    IO.puts "=="
+    hack(new_words)
+  end
 
   def score_words(""),    do: :error
   def score_words(words), do: Enum.zip(word_score_keys(words), word_score_values(words)) |> to_map
 
-  def word_score_keys(words),       do: words |> to_list |> Enum.map &(String.to_atom(&1))
+  def word_score_keys(words),       do: words |> to_list
   def word_score_values(words),     do: words |> to_list |> Enum.map &(score_word(&1, count_chars(words)))
 
   def score_word(word, map),        do: word |> String.codepoints |> score_word(map, 0)
@@ -16,8 +45,11 @@ defmodule Hack do
   def score_letter({:ok, value}),   do: value
   def score_letter({:error}),       do: 0
 
-  def to_list(string), do: String.split(string, [" ", ","], trim: true)
-  def to_map(kw_list), do: Enum.into(kw_list, %{})
+  def best_word(scored_words), do: scored_words |> Enum.max_by fn({_, v}) -> v end
+
+  def to_list(string),              do: String.split(string, [" ", ","], trim: true)
+  def to_map(kw_list),              do: Enum.into(kw_list, %{})
+  def list_to_string(list),         do: Enum.map(list, &("#{&1} ")) |> List.to_string |> String.rstrip
   def alphabet_map(value \\ 0),     do: Enum.into ?a..?z, %{}, &{String.to_atom(<<&1>>), value}
 
   def count_chars(s),         do: s |> String.replace(" ", "") |> String.codepoints |> count_chars(alphabet_map)
@@ -28,7 +60,7 @@ defmodule Hack do
   end
 end
 
-defmodule F4 do
+defmodule F4Old do
   # def matches_all(map, string) do
   #   Enum.all? map, &F4.match?(&1, string)
   # end
